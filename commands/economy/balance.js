0ede16c8d4fe68d1a61c27c778491a2664b3c033
EnderMcOfficial <79957263+EnderMcOfficial@users.noreply.github.com>
2021-09-02 09:08:26 +0700

    Create balance.js

---
+++ commands/economy/balance.js
@@ -0,0 +1,11 @@
+module.exports = ({
+    name: "balance",
+    aliases: ['bal', 'money', 'cash'],
+    description: "See your or a user's balance",
+    usage: "balance (user)",
+    code: `$color[RANDOM]
+    $title[$username[$findMember[$message]]'s balance]
+    $description[<@$findMember[$message]> currently has $getGlobalUserVar[money;$findMember[$message]]💴.]
+    $cooldown[5s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}{delete:$getCooldownTime[globalUser;balance;$authorID;yes]ms}]
+    `
+})
