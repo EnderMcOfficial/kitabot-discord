f1ca0f5f4f7124906040e7b70a3e9bf4eea3bb8b
EnderMcOfficial <79957263+EnderMcOfficial@users.noreply.github.com>
2021-09-02 09:15:38 +0700

    Create ip.js

---
+++ commands/info/ip.js
@@ -0,0 +1,16 @@
+module.exports = ({
+    name: "ip",
+    aliases: ['ipsrv','ipsvr'],
+    code: `
+    $thumbnail[$serverIcon]
+    $author[$serverName;$serverIcon]
+    $title[Server Mc Ip]
+    $description[
+Ip:
+\`\`\`
+KitaNetwork.mcalias.com
+\`\`\`
+]
+$footer[Ip Server Mc KitaNetwork]
+$color[BLUE]`
+})
